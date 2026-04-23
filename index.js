8955a66c66272fa8572cc75fbce8901d4c8d17de
LiuShen <01@liushen.fun>
2026-04-23 23:14:20 +0800

    😊初步实现功能

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ hexo.config.feed = Object.assign({
   content: true,
   content_limit: 140,
   content_limit_delim: '',
+  pretty_rss2_file: '',
+  pretty_atom_file: '',
   order_by: '-date',
   autodiscovery: true
 }, hexo.config.feed);
